c0cc864666be5094ad8df8fc28e0fbd15c60f036
Sarah Smruthi <smruthisarah@gmail.com>
2023-12-26 10:02:49 +0530

    commit on model

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,11 @@
 
 print("adding all the data processing code related to fraud detection")
 
+a=5
+b=10
+def sum(a,b):
+    c = a+b
+    return c
+
+print("added a new function sum which is not correct")
+
